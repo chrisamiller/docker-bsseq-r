a83354b2b24ecd3a6b61203cc98ca684c4964056
Chris Miller <chrisamiller@gmail.com>
2019-01-21 22:04:51 -0600

    adding gplots

--- rpackages.R
+++ rpackages.R
@@ -1,4 +1,4 @@
 source("https://bioconductor.org/biocLite.R")
 biocLite()
 biocLite(c("bsseq","data.table","GenomicRanges","methylKit","readr","sqldf","stringr"))
-install.packages(c("tidyverse","ggplot2","Hmisc","plotrix","png"),repo=paste0("https://mran.microsoft.com/snapshot/",format(Sys.Date(), format="%Y-%m-%d")))
+install.packages(c("tidyverse","ggplot2","Hmisc","plotrix","png","gplots"),repo=paste0("https://mran.microsoft.com/snapshot/",format(Sys.Date(), format="%Y-%m-%d")))
